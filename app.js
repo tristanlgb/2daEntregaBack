fc3a7dd8c4e8cafef4f5def6b291f3abb60918ba
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-04-02 20:20:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const express = require('express');
 const ProductManager = require('./ProductManager');
 
 const app = express();
-
 const productManager = new ProductManager();
 productManager.readProductsFromFile();
 
 app.get('/products', (req, res) => {
   const limit = req.query.limit ? parseInt(req.query.limit) : null;
   const products = productManager.getProducts(limit);
-  res.send({ products });
+  res.json(products);
 });
+
 
 app.get('/products/:pid', (req, res) => {
   const pid = req.params.pid;
   const product = productManager.getProductById(pid);
-  res.send({ product });
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Product not found');
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
