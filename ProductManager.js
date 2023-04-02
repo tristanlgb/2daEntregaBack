e9bcdef8eb3a34a04dadce0f0cb39f2701f3c654
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-04-02 20:20:23 -0300

    Update ProductManager.js

--- ProductManager.js
+++ ProductManager.js
@@ -3,14 +3,13 @@ class ProductManager {
     this.products = [];
   }
 
-  // Function to read the products from file (assuming the file is called 'products.json')
   readProductsFromFile() {
     const fs = require('fs');
     const data = fs.readFileSync('products.json');
     this.products = JSON.parse(data);
   }
 
-  // Function to get all products
+
   getProducts(limit = null) {
     if (limit) {
       return this.products.slice(0, limit);
@@ -19,7 +18,7 @@ class ProductManager {
     }
   }
 
-  // Function to get a specific product by ID
+
   getProductById(id) {
     return this.products.find(product => product.id === id);
   }
